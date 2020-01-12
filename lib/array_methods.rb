e6b2e7fe9b64c309cb818a8b2cf141d8b9a96724
Joe Gelay <joegelay@gmail.com>
2020-01-12 18:47:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,15 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  
+  counter = 0 
+  max_value = 0
+  while counter < array.length do
+    if (array[counter] > max_value)
+      
+    
+    
+    counter += 1 
+  end
   
 end
 
