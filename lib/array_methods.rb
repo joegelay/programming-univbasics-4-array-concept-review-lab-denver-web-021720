ed6d5a06c23cb47d795ff7b89125101dbaea3e43
Joe Gelay <joegelay@gmail.com>
2020-01-12 18:54:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -20,7 +20,7 @@ def find_min_value(array)
   counter = 0 
   min_value = 0 
   while counter < array.length do
-    if (array[counter] < min_value)
+    if ((array[counter] < min_value) || counter == 0)
       min_value = array[counter]
     end
     
